f29ed98dadd997f688d2f43422722890e1761115
Lucas Rivelles <lucas.rivelles@glovoapp.com>
2024-02-02 16:42:44 +0100

    chore: Replace with MySQL

---
+++ mysql-init-files/create_tables.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE bank;
+USE bank;
+
+CREATE TABLE IF NOT EXISTS accounts (
+  id INT NOT NULL,
+  customer_id varchar(250) NOT NULL,
+  balance DECIMAL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS loans (
+  id INT NOT NULL,
+  account_id INT NOT NULL,
+  ammount DECIMAL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_account
+    FOREIGN KEY(id)
+    REFERENCES accounts(id)
+);
+
+INSERT INTO accounts
+(id, customer_id, balance)
+VALUES
+(1, 10, 1000.0),
+(2, 20, 1000.0),
+(3, 30, 1000.0),
+(4, 40, 1000.0),
+(5, 50, 1000.0);
--- sql/create_tables.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS product (
-  product_id INT NOT NULL,
-  name varchar(250) NOT NULL,
-  price DECIMAL,
-  PRIMARY KEY (product_id)
-);
